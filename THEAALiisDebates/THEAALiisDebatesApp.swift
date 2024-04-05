e60aeaae7bf3fae283d816d6bab84a3093580e46
Ali <ali.theaalii@gmail.com>
2024-04-05 13:25:37 -0700

    Initial Commit

---
+++ THEAALiisDebates/THEAALiisDebatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  THEAALiisDebatesApp.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct THEAALiisDebatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
