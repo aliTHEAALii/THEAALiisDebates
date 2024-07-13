d766254d7eeadaa6b953cee06506ffb82b972fb7
aliTHEAALii <160378009+aliTHEAALii@users.noreply.github.com>
2024-07-13 07:38:36 -0700

    Added -
    - ChainLink from VL
    - Voting Buttons to Side Sheet

---
+++ THEAALiisDebates/VIEW/TI/Control Center/CCBottomBar.swift
@@ -0,0 +1,18 @@
+//
+//  CCBottomBar.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 7/13/24.
+//
+
+import SwiftUI
+
+struct CCBottomBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CCBottomBar()
+}
---
+++ THEAALiisDebates/VIEW/TI/Ti Vertical List/Voting Post Card SVs/VotingButtonsSV.swift
@@ -0,0 +1,18 @@
+//
+//  VotingButtonsSV.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 7/13/24.
+//
+
+import SwiftUI
+
+struct VotingButtonsSV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VotingButtonsSV()
+}
