f93ceeca6341f81f42f95e105df685bc52aeb13d
aliTHEAALii <160378009+aliTHEAALii@users.noreply.github.com>
2024-07-22 06:38:24 -0700

    Next - UserButton @State var user

---
+++ THEAALiisDebates/Globals/Shapes/SideSheetRectangle.swift
@@ -0,0 +1,18 @@
+//
+//  SideSheetRectangle.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 7/16/24.
+//
+
+import SwiftUI
+
+struct SideSheetRectangle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideSheetRectangle()
+}
---
+++ THEAALiisDebates/VIEW/TI/Control Center/ii - Interaction Information/ii Components/PickIntroVideoButton.swift
@@ -0,0 +1,18 @@
+//
+//  PickIntroVideoButton.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 7/15/24.
+//
+
+import SwiftUI
+
+struct PickIntroVideoButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickIntroVideoButton()
+}
