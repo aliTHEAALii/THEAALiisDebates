aa668f2250a4ae1a28114d8f18aaf8fa613f54f5
aliTHEAALii <ali.THEAALii@gmail.ceom>
2024-07-28 13:42:26 -0700

    Tab Search View Start

---
+++ THEAALiisDebates/VIEW/Tab 2 Search/TabSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  TabSearchView.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 7/28/24.
+//
+
+import SwiftUI
+
+struct TabSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabSearchView()
+}
