e63639a06ecb71dd04c281eeccc0491ae9e64aba
aliTHEAALii <ali.THEAALii@gmail.ceom>
2024-07-31 03:29:06 -0700

    ti Post Content View

---
+++ THEAALiisDebates/VIEW/TI/TiPostContentView.swift
@@ -0,0 +1,18 @@
+//
+//  TiPostContentView.swift
+//  THEAALiisDebates
+//
+//  Created by Ali Kadhum on 7/31/24.
+//
+
+import SwiftUI
+
+struct TiPostContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TiPostContentView()
+}
